b7b66cd54d66267ad5dd210d75d191c556facfe2
Zale Hren <zale.hren@columbiachristian.com>
2025-11-17 09:16:32 -0800

    Pipe v8 (pipe random)

--- Pipe.js
+++ Pipe.js
@@ -9,7 +9,7 @@ const PipeState = {
 export class Pipe {
     constructor(speed, startingPOS) {
         this.startingPOS = startingPOS
-        this.yCenter = 0
+        this.y = 0
         this.opening = 200
         this.dx = speed
         this.dy = 0
@@ -17,44 +17,49 @@ export class Pipe {
         this.img = new Image();
         this.img.src = 'pipe.png';
         this.setPipeState(PipeState.IDLE)
-        this.xRandom
+        this.xRandom = 0
+        this.yRandom = 0
     }    
     draw(ctx, i) {
         if (this.state != PipeState.IDLE) {
-            ctx.drawImage(this.img, this.x, this.yCenter + (this.opening/2))
+            ctx.drawImage(this.img, (this.x +this.xRandom), (this.y + this.yRandom)+ (this.opening/2))
             ctx.save()
-            ctx.translate(this.x, this.yCenter - (this.opening/2))
+            ctx.translate((this.x +this.xRandom), (this.y+ this.yRandom) - (this.opening/2))
             ctx.scale(1,-1)
             ctx.drawImage(this.img, 0, 0 )
             ctx.restore()
-            ctx.fillText(`${this.startingPOS}`, this.x, this.yCenter)
+            ctx.fillText(`${this.startingPOS}`, this.x, this.y)
+
         }
+    }
+
+    upperboundingBox() {
+        return {
+            x:(this.x +this.xRandom),
+            y: this.y -(this.opening/2)-this.img.height + this.yRandom,
+            width: this.img.width,
+            height: this.img.height
+        }
+    }
+
+    lowerboundingBox() {
+        return {
+             x: (this.x +this.xRandom),
+             width: this.img.width, 
+             y: this.y +(this.opening/2) + this.yRandom,
+              height: this.img.height
+            }
     }
 
     animate(){
-        this.yCenter += this.dy
+        this.y += this.dy
         this.x += this.dx
         this.x = (this.x + this.dx)
         if(this.x <= -200){
-            this.x +=1260
+            this.x +=1260 
+            this.yRandom = Math.random()*400-200
         }
 
-    }
-
-    upperboundingBox() {
-        return { x: this.x, width: this.img.width, y: this.yCenter -(this.opening/2)-this.img.height, height: this.img.height }
-    }
-
-    lowerboundingBox() {
-        return { x: this.x, width: this.img.width, y: this.yCenter +(this.opening/2), height: this.img.height }
-    }
-
-    upperboundingBox() {
-        return { x: this.x, width: this.img.width, y: this.yCenter -(this.opening/2)-this.img.height, height: this.img.height }
-    }
-
-    lowerboundingBox() {
-        return { x: this.x, width: this.img.width, y: this.yCenter +(this.opening/2), height: this.img.height }
     }
 
     startMoving(){
@@ -68,8 +73,9 @@ export class Pipe {
         console.log(`set pipe state to ${state}`)
         if (state == PipeState.IDLE){
             this.x = this.startingPOS
-            this.yCenter =  160+Math.random()*400 
+            this.y = 360
             this.dx = 0
+            this.xRandom = 50+Math.random()*100
         }else if (state == PipeState.PLAYING){
           this.dx = -2 
         } else if (state == PipeState.GAMEOVER){
